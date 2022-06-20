7e95ba5d798b86df3476a47bf665df8a7028334a
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-20 16:42:58 +0100

    Using Link to navigate through pre-rendered pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function NewsPage() {
-  return <h1>This is the news page</h1>;
+  return (
+    <>
+      <h1>This is the News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/Nextjs-is-cool">NextJS is cool</Link>
+        </li>
+        <li>
+          <Link href="/news/Nextjs-is-awesome">NextJS is awesome</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
