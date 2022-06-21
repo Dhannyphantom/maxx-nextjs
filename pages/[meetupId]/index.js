9553c412289a2713a09fd126894d4c0615e5f5a5
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-21 19:04:31 +0100

    Changed img HTML elements to Image and adopted the getStaticProps in the dynamic page but not functional

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,30 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
+export async function getStaticProps(context) {
+  // fetch single data from an API
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some address",
+        description: "This is some description",
+        image: "/images/avatarrs.jpg",
+      },
+    },
+  };
+}
+
 export default function MeetupDetailPage() {
   return (
     <MeetupDetail
       title="First Meetup"
       address="Some address"
       description="This is some description"
-      image="../images/avatarrs.jpg"
+      image="/images/avatarrs.jpg"
     />
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,46 +4,34 @@ const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "First Meetup",
-    image: "./images/avatarrs.jpg",
+    image: "/images/avatarrs.jpg",
     address: "Some address, some city 4323",
     description: "This is the first address",
   },
   {
     id: "m2",
     title: "Second Meetup",
-    image: "images/avatarrs.jpg",
+    image: "/images/avatarrs.jpg",
     address: "Some address, some city 4323",
     description: "This is the second address",
   },
   {
     id: "m3",
     title: "Third Meetup",
-    image: "images/avatarrs.jpg",
+    image: "/images/avatarrs.jpg",
     address: "Some address, some city 4323",
     description: "This is the third address",
   },
 ];
 
-// export async function getStaticProps() {
-//   // fetch some data from API
-
-//   return {
-//     props: {
-//       meetupsData: DUMMY_MEETUPS,
-//     },
-//     revalidate: 10, // THIS WILL ALWAYS REGENERATE THIS PAGE EVERY TEN SECS
-//     //              // IN THE SERVER SO LONG REQUESTS ARE COMING IN.
-//   };
-// }
-
-export async function getServerSideProps() {
-  // fetch data from an API
-  // THIS IS ALWAYS GENERATED ON EVERY REQUEST MADE TO THE SERVER
+export async function getStaticProps() {
+  // fetch some data from API
 
   return {
     props: {
       meetupsData: DUMMY_MEETUPS,
     },
+    revalidate: 10,
   };
 }
 
