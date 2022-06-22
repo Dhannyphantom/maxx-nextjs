a3ec61badca33f662b515ef2e5461836453ff0af
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-22 18:12:36 +0100

    Added the new meetups data to mongodb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,17 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
-  const onAddNewMeetupHandler = (meetupData) => {
-    console.log(meetupData);
+  const onAddNewMeetupHandler = async (meetupData) => {
+    const res = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
   };
   return <NewMeetupForm onAddMeetup={onAddNewMeetupHandler} />;
 }
