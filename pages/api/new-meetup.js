1d45a8fdfe5e26eb20ccd74e7d0248820726df1f
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-06-22 17:59:08 +0100

    Added nanoid, fontawesome, mongodb, mongoose packages and completed the first test api route

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,22 @@
-// THIS API ROUTE IS ACCESSED VIA api/new-meetup
+import { MongoClient } from "mongodb";
+// this is an api route -- /api/new-meetup
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb://localhost:27017/meetups"
+    );
+    const db = client.db();
+
+    const meetupsCollection = await db.createCollection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
